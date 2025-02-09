21166b9ad32124e5675fc25275cb896f45d8038a
Bruno Ferreira <fbruno233@gmail.com>
2025-02-08 21:20:12 -0300

    add: edit and delete buttons

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -1,13 +1,26 @@
-import './draftEditor.css';
+import { useState } from 'react';
+import './paragraph.css';
 
 interface ParagraphProps {
   paragraph: string;
 }
 
 export default function Paragraph({paragraph}: ParagraphProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <p className="paragraph">
-      {paragraph}
-    </p>
-  )
-}
+    <div
+      className="paragraph"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <p>{paragraph}</p>
+      {isHovered && (
+        <div className="paragraph-buttons">
+          <button className="edit-button">Editar</button>
+          <button className="delete-button">Excluir</button>
+        </div>
+      )}
+    </div>
+  );
+};
